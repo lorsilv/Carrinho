de5f94eab41fb80fde84684361e8bb14124d35b0
Lorrayne Silva <lorrayne.mara@hotmail.com>
2022-09-02 11:54:19 -0300

    Primeiro commit

---
+++ carrinho.py
@@ -0,0 +1,7 @@
+cart = []
+
+id_user = input('Insira o ID do usuário:')
+id_produto = input('Insira o id do produto:')
+price_produto = float(input('Insira o preço do produto:'))
+quantity_produto = int(input('Insira a quantidade do produto:'))
+
